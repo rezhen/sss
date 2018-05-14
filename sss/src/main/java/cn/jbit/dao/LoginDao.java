faed55dc22cf59cf6eaa0030b28bd8c7d21f35e7
hcl <3031842345@qq.com>
2018-05-14 10:48:26 +0800

    Revert "111"
    This reverts commit 9bb32869bff6d849558f0c7bcef3efedf71817d7.

--- sss/src/main/java/cn/jbit/dao/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package cn.jbit.dao;
-
-import cn.jbit.entity.Users;
-
-public interface LoginDao {
-	
-	public Users Login(String name,String pass);
-
-}
--- sss/src/main/java/cn/jbit/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package cn.jbit.service;
-
-import cn.jbit.entity.Users;
-
-public interface LoginService {
-	
-	public Users Login(String name,String pass);
-
-}
